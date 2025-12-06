a344362b4dc53716f16df6b02bc688c59c816210
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2025-12-06 20:16:45 +0530

    restore local work

--- .next/dev/static/chunks/app_(auth)_sign-in_page_tsx_2820e0c9._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_19959588._.js",
-  "static/chunks/_c5689177._.js"
-],
-    source: "dynamic"
-});
--- .next/dev/static/chunks/app_(root)_page_tsx_2820e0c9._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_8f0ffc2a._.js",
-  "static/chunks/_a737256c._.js"
-],
-    source: "dynamic"
-});
